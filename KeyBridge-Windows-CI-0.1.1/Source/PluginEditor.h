cf01cf3d500b74565f870ae0fa1f4df704ce2cdb
murdermittenmedia-dotcom <2.93378784e+08+murdermittenmedia-dotcom@users.noreply.github.com>
2026-08-25 17:01:15 +0000

    Use minimal lifecycle-safe VST3 editor

--- KeyBridge-Windows-CI-0.1.1/Source/PluginEditor.h
+++ KeyBridge-Windows-CI-0.1.1/Source/PluginEditor.h
@@ -2,22 +2,18 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
-class KeyBridgeAudioProcessorEditor : public juce::AudioProcessorEditor, private juce::Timer
+class KeyBridgeAudioProcessorEditor final : public juce::AudioProcessorEditor
 {
 public:
     explicit KeyBridgeAudioProcessorEditor (KeyBridgeAudioProcessor&);
     ~KeyBridgeAudioProcessorEditor() override = default;
+
     void paint (juce::Graphics&) override;
     void resized() override;
 
 private:
-    void timerCallback() override;
-    void playReferenceTone (int midiNote);
     KeyBridgeAudioProcessor& processor;
-    juce::Label title, keyLabel, bpmLabel, confidenceLabel, notesLabel, recommendationLabel;
-    juce::ComboBox profileBox, genreBox, vibeBox;
-    juce::Label profileLabel, genreLabel, vibeLabel;
-    juce::TextButton analyzeButton { "ANALYZE" }, holdButton { "HOLD" };
-    std::array<std::unique_ptr<juce::TextButton>, 12> noteButtons;
+    juce::Label statusLabel;
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (KeyBridgeAudioProcessorEditor)
 };
